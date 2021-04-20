6e844c521a784bcbc92d60545a4cae3701cc37e9
CoforgeAbhyudaya <atandon1412@gmail.com>
2021-04-20 15:53:32 +0530

    merging

---
+++ Employee.java
@@ -0,0 +1,10 @@
+public class Employee{
+	static String name="Abhyu";
+	static int id=12;
+	public static void main(String emp[])
+	{
+		System.out.println("Employee Name"+Employee.name);
+		System.out.println("Employee ID"+Employee.id);
+
+	}
+}
